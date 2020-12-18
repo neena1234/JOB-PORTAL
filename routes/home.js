5305eed7d5db477b6d83c5a2dc9a959e5570c688
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-12-18 23:45:18 +0530

    adding login - signup tp home

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userHelper = require('../helpers/account-helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,4 +16,42 @@ router.get('/about', function (req, res, next) {
 router.get('/services', function (req, res, next) {
     res.render('home/services')
 });
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/')
+    } else {
+        res.render('home/login', {"loginErr": req.session.loginErr})
+        req.session.loginErr = false
+    }
+})
+
+router.get('/signup', (req, res) => {
+    res.render('home/signup')
+})
+
+router.post('/signup', (req, res) => {
+    userHelper.doSignup(req.body).then((response) => {
+        req.session.loggedIn = true
+        req.session.user = response
+        res.redirect('/')
+    })
+})
+
+router.post('/login', (req, res) => {
+    userHelper.doLogin(req.body).then((response) => {
+        if (response.status) {
+            req.session.loggedIn = true
+            req.session.user = response.user
+            res.redirect('/user')
+        } else {
+            req.session.loginErr = "Invalid username or password"
+            res.redirect('/login')
+        }
+    })
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+})
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -11,45 +11,6 @@ router.get('/', function (req, res, next) {
     })
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/')
-    } else {
-        res.render('user/login', {"loginErr": req.session.loginErr})
-        req.session.loginErr = false
-    }
-})
-
-router.get('/signup', (req, res) => {
-    res.render('user/signup')
-})
-
-router.post('/signup', (req, res) => {
-    userHelper.doSignup(req.body).then((response) => {
-        req.session.loggedIn = true
-        req.session.user = response
-        res.redirect('/')
-    })
-})
-
-router.post('/login', (req, res) => {
-    userHelper.doLogin(req.body).then((response) => {
-        if (response.status) {
-            req.session.loggedIn = true
-            req.session.user = response.user
-            res.redirect('/')
-        } else {
-            req.session.loginErr = "Invalid username or password"
-            res.redirect('/login')
-        }
-    })
-})
-
-router.get('/logout', (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
-})
-
 router.get('/apply',function(req,res){
 res.render('user/apply')
 })
