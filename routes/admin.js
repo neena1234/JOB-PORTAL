80dae647bab5f8612d372c56608325daa7eaa367
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-12-26 11:14:49 +0530

    adding the templates in the admin user

--- routes/admin.js
+++ routes/admin.js
@@ -3,31 +3,30 @@ var router = express.Router();
 
 var express = require('express');
 const {render} = require('../app');
-var jobHelper = require('../helpers/job-helpers');
 
-var MongoClient = require('mongodb').MongoClient
-
-/* GET user listing. */
-router.get('/', function (req, res, next) {
-    jobHelper.getAllJobs().then((jobs) => {
-        res.render('admin/view-jobs', {jobs: jobs})
-    })
+router.get('/manage-jobs', function (req, res) {
+    let user = req.session.user
+    res.render('admin/all-jobs', {user, admin: true, employer: false, applicant: false, home: false})
 });
 
-router.get('/admin-dashboard',function (req,res){
+router.get('/admin-dashboard', function (req, res) {
     let user = req.session.user
-    res.render('admin/admin-dashboard',{user,admin:true,employer:false,applicant:false,home:false})
+    res.render('admin/admin-dashboard', {user, admin: true, employer: false, applicant: false, home: false})
 })
 
-
-router.get('/add-job', function (req, res) {
-    res.render('admin/add-job')
+router.get('/view-stats', function (req, res) {
+    let user = req.session.user
+    res.render('admin/view-stats', {user, admin: true, employer: false, applicant: false, home: false})
 });
 
-router.post('/add-jobs', ((req, res) => {
-    jobHelper.addJobs(req.body, (result) => {
-        res.render('admin/add-job')
-    })
-}))
+router.get('/manage-employers', function (req, res) {
+    let user = req.session.user
+    res.render('admin/view-all-employers', {user, admin: true, employer: false, applicant: false, home: false})
+});
+
+router.get('/manage-users', function (req, res) {
+    let user = req.session.user
+    res.render('admin/view-all-users', {user, admin: true, employer: false, applicant: false, home: false})
+});
 
 module.exports = router;
