59d570b84f37ef88a6466effa4fcc76867887b82
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-12-26 11:26:48 +0530

    removing redudant code

--- app.js
+++ app.js
@@ -49,11 +49,6 @@ app.use(function (req, res, next) {
 });
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
